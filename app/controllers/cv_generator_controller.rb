8253f5ded8c06f499abf0a4c567c3a749b47d15f
Alexandre Loreilei Poirot <alexandre.poirot@gmail.com>
2018-05-16 00:29:53 +0200

    Added possibility to have LinkedIn profile link in personnal info. Added possibility to upload a portrait picture.

--- app/controllers/cv_generator_controller.rb
+++ app/controllers/cv_generator_controller.rb
@@ -8,10 +8,17 @@ class CvGeneratorController < ApplicationController
  	
   	json_content = params[:resume_json_file].read()
   	@cv = JSON.parse(json_content)
-	@i18n = i18n[@cv["i18n"]]
 
-	@header = @cv["header"]
-	@skills = @cv["skills"]
-	@content = @cv["content"]
+    unless params[:portrait_image_file].nil?
+      portrait_img_file_content = params[:portrait_image_file].read()
+      @portrait_image_type = params[:portrait_image_file].content_type
+      @portrait_image_b64 = Base64.encode64(portrait_img_file_content)
+    end
+
+    @i18n = i18n[@cv["i18n"]]
+
+    @header = @cv["header"]
+    @skills = @cv["skills"]
+    @content = @cv["content"]
   end
 end
